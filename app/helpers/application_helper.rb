d975fdd0deab16bace5262d1876c25a95ff5ef13
conorprunty <conorprunty@hotmail.com>
2017-04-06 20:30:10 +0100

    t

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 ##http://railscasts.com/episodes/228-sortable-table-columns
 module ApplicationHelper
  def sortable(column, title = nil)
-     title ||= (column.titleize)     
+     @column = "Name"
+     title ||= (@column.titleize)     
      direction = column == sort_column && sort_direction== "asc" ? "desc" : "asc"    
      link_to title, :sort => column, :direction => direction
    end
